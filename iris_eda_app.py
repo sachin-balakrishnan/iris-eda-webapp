f4f8f1285b22aa0fba51412978d7ffb16d17d29e
Sachin Balakrishnan <balaksac@amazon.com>
2022-05-04 11:07:59 -0400

    Iris eda app deployment

---
+++ iris_eda_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+def main():
+	"""Simple iris eda app"""
+
+	st.title("Iris EDA app with streamlit")
+	st.subheader("Streamlit is cool")
+
+
+if __name__ == "__main__":
+	main()
